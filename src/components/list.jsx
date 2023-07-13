72c0782d896d8777573cf460631904adcfc1ffa5
Alex Zyra <117516145+AlexZyra@users.noreply.github.com>
2023-07-13 17:19:58 +0300

    hmhmhm
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	new file:   src/components/list.jsx

---
+++ src/components/list.jsx
@@ -0,0 +1,12 @@
+const List = (props) => {
+    const { items } = props;
+    return (
+        <ul>
+            {items.map((i) => (
+                <li>{i}</li>
+            ))}
+        </ul>
+    )
+}
+
+export default List
